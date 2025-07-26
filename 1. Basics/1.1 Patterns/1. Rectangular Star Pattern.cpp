dec022511bbe4a0d81763039c588c1d9940d2ea3
mansiey <mansi46rd@gmail.com>
2025-07-26 22:42:15 +0530

    adding to patterns

---
+++ 1. Basics/1.1 Patterns/1. Rectangular Star Pattern.cpp
@@ -0,0 +1,18 @@
+// 🔹 Question: Rectangular Star Pattern
+//  🔗 Link: https://www.naukri.com/code360/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
+//  ✍️ Approach: Simple nested loops to print rectangle of stars
+// ** NOTE **
+// 🔍 Only function provided by GFG, no main() required 
+
+// void nForest(int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             cout << "* ";
+//         }
+
+//         cout << endl;
+//     }
+// }
