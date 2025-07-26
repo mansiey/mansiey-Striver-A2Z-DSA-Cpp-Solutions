a51f47ff2166b213d3f1419ca8121b92f910a9e5
mansiey <129222429+mansiey@users.noreply.github.com>
2025-07-26 23:09:36 +0530

    Update 1. Rectangular Star Pattern.cpp

--- 1. Basics/1.1 Patterns/1. Rectangular Star Pattern.cpp
+++ 1. Basics/1.1 Patterns/1. Rectangular Star Pattern.cpp
@@ -1,5 +1,5 @@
 // 🔹 Question: Rectangular Star Pattern
- 🔗 Link: https://www.naukri.com/code360/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems
+// 🔗 Link:(https://www.naukri.com/code360/problems/n-forest_6570177?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_patternproblems)
 //  ✍️ Approach: Simple nested loops to print rectangle of stars
 // ** NOTE **
 // 🔍 Only function provided by GFG, no main() required 
