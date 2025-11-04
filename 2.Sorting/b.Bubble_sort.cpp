b37807fd7cb12bcd7ae8afd1957c2014d6ce26c5
mansiey <mansi46rd@gmail.com>
2025-11-04 12:02:07 +0530

    Added new sorting Question

---
+++ 2.Sorting/b.Bubble_sort.cpp
@@ -0,0 +1,38 @@
+/* Question - 1: BUBBLE SORT
+Given an array of integers called nums,
+sort the array in non-decreasing order using the bubble sort algorithm and return the sorted array.
+
+Time Complexity: O(n²)
+Space Complexity: O(1)
+
+Code:
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> bubbleSort(vector<int>& nums) {
+        int n = nums.size();
+        bool didswap = false;
+        for(int i = n-1; i > 0; i--)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(nums[j] > nums[j+1])
+                {
+                    swap(nums[j], nums[j+1]);
+                    didswap = true;
+                }
+            }
+            //if no swapping done, that is the array is already sorted.
+            //break out of loop.
+            if(!didswap){
+                break;
+            }
+        }
+        return nums;
+
+    }
+};
